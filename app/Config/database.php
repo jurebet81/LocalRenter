40dfdfe8e773e9b86df78869ab47616d6522345b
jurebet81 <julianrpo2711@hotmail.com>
2015-10-21 23:32:11 -0500

    Create database.php

---
+++ app/Config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'adminL1aANv1',
+		'password' => 'Y3Efw8-tKMDE',
+		'database' => 'php',
+		'prefix' => '',
+		'encoding' => 'utf8',
+		
+	);
+	
+	}
